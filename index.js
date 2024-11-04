53e02265df6ca9370ddae55e94ab50bdccaddb44
vedernikovaeva <165674837+vedernikovaeva@users.noreply.github.com>
2024-11-04 22:35:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,94 @@
-import { createRouter, createWebHistory } from "vue-router"; // Зміна імпортів
-import SongsListPage from "@/views/SongListView.vue"; // Головна сторінка зі списком пісень
-import AddSongPage from "@/views/AddSongView.vue"; // Сторінка для додавання нової пісні
+import { createStore } from "vuex";
 
-const routes = [
-  {
-    path: "/",
-    name: "SongsList",
-    component: SongsListPage,
+export default createStore({
+  state: {
+    songs: [
+      {
+        id: 1,
+        title: "Sky",
+        artist: "Playboi Carti",
+        album: "Whole Lotta Red",
+      },
+      {
+        id: 2,
+        title: "Rich Flex",
+        artist: "Drake & 21 Savage",
+        album: "Her Loss",
+      },
+      {
+        id: 3,
+        title: "First Class",
+        artist: "Jack Harlow",
+        album: "Come Home The Kids Miss You",
+      },
+      {
+        id: 4,
+        title: "Bad Habit",
+        artist: "Steve Lacy",
+        album: "Gemini Rights",
+      },
+      {
+        id: 5,
+        title: "As It Was",
+        artist: "Harry Styles",
+        album: "Harry's House",
+      },
+      {
+        id: 6,
+        title: "Kill Bill",
+        artist: "SZA",
+        album: "SOS",
+      },
+      {
+        id: 7,
+        title: "Creepin'",
+        artist: "Metro Boomin, The Weeknd & 21 Savage",
+        album: "Heroes & Villains",
+      },
+      {
+        id: 8,
+        title: "Superhero (Heroes & Villains)",
+        artist: "Metro Boomin ft. Future & Chris Brown",
+        album: "Heroes & Villains",
+      },
+      {
+        id: 9,
+        title: "I Ain't Worried",
+        artist: "OneRepublic",
+        album: "Top Gun: Maverick (Music from the Motion Picture)",
+      },
+      {
+        id: 10,
+        title: "Calm Down",
+        artist: "Rema & Selena Gomez",
+        album: "Rave & Roses",
+      },
+    ],
   },
-  {
-    path: "/add-song",
-    name: "AddSong",
-    component: AddSongPage,
+  mutations: {
+    ADD_SONG(state, song) {
+      state.songs.push(song);
+    },
+    DELETE_SONG(state, songId) {
+      state.songs = state.songs.filter((song) => song.id !== songId);
+    },
   },
-];
+  actions: {
+    addSong({ commit }, song) {
+      commit("ADD_SONG", song);
+    },
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    deleteSong({ commit }, songId) {
+      commit("DELETE_SONG", songId);
+    },
+  },
+  modules: {},
+  getters: {
+    getSongs: (state) => state.songs,
+    getSongById: (state) => (id) => state.songs.find((song) => song.id === id),
+    getSongsByArtist: (state) => (artist) =>
+      state.songs.filter((song) => song.artist === artist),
+    getSongsByAlbum: (state) => (album) =>
+      state.songs.filter((song) => song.album === album),
+  },
 });
-
-export default router;
