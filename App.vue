768d18bf00d5f551dd17d0105408139de85ed798
vedernikovaeva <165674837+vedernikovaeva@users.noreply.github.com>
2024-11-04 22:34:41 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
